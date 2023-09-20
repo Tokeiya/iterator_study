3bf3218e9c3b2a5d330524bbe064439d77a6ba86
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2023-09-20 20:47:30 +0900

    partial impl.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+#![feature(iter_array_chunks)]
+
 mod clip;
 mod winnow;
 
-fn main() {}
+fn main() {
+	let chunk = (0..9).array_chunks::<5>();
+
+	for clip in chunk {
+		println!("----------");
+		for i in clip {
+			println!("{}", i)
+		}
+	}
+}
